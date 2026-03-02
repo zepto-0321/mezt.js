668b2e9bd919082c6e2603edc34739933bd707a4
zemyonpto <yiyoushiye@gmail.com>
2026-03-02 10:07:33 +0900

    index.js
    In order to use this engine, plz import this file

---
+++ index.js
@@ -0,0 +1,6 @@
+//このファイルをimportしてつかってネ
+
+export { Game, GameContext } from "./core/game.js";
+export { Scene } from "./core/scene.js";
+export { SceneManager } from "./core/sceneManager.js";
+export { Input } from "./core/input.js";
